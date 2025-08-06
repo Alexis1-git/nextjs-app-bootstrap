42777490990272b8ebef574c5e1b5fd5c4bbf7db
Alexis1-git <alexisrwibasira@gmail.com>
2025-08-06 13:37:08 +0200

    Add .next/static/chunks/src_app_products_page_tsx_f65b8abc._.js 1754480224017

---
+++ .next/static/chunks/src_app_products_page_tsx_f65b8abc._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a00dc99f._.js",
+  "static/chunks/src_b03a8b76._.js"
+],
+    source: "dynamic"
+});
